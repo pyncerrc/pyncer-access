8b1b2d209d6a4bce8c2825d54b52da6a080697a2
pyncer <root@pyncer.com>
2026-06-16 18:15:35 -0400

    Added missing getToken() method to BearerAuthenticatorInterface.

--- src/BearerAuthenticatorInterface.php
+++ src/BearerAuthenticatorInterface.php
@@ -6,6 +6,6 @@ use Pyncer\Data\Model\ModelInterface;
 
 interface BearerAuthenticatorInterface extends AuthenticatorInterface
 {
-    public function getToken(): ?ModelInterface
+    public function getToken(): ?ModelInterface;
     public function authenticate(string $token): bool;
 }
